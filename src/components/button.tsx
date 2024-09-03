79bcd174c5c76c278316d148e7253c617288858c
lucas vinicius <lucassilved@gmail.com>
2024-09-02 21:58:16 -0300

    feat: add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,41 @@
+import { HTMLAttributes, ReactNode } from 'react';
+import { tv, type VariantProps } from 'tailwind-variants';
+
+const button = tv({
+  base: 'border-none rounded-md p-1 min-w-16 min-h-8 text-gray-200 font-bold hover:opacity-80',
+  variants: {
+    size: {
+      sm: 'text-sm',
+      md: 'w-40 h-10 text-md',
+      lg: 'w-80 h-10 text-lg',
+      full: 'w-full h-10 text-lg',
+    },
+    variant: {
+      primary: 'bg-cyan-600',
+      outline: 'bg-none border border-solid border-gray-500',
+    },
+    defaultVariants: {
+      size: 'md',
+      variant: 'primary',
+    },
+  },
+});
+
+type ButtonVariants = VariantProps<typeof button>;
+
+interface ButtonProps
+  extends HTMLAttributes<HTMLButtonElement>,
+    ButtonVariants {
+  children: ReactNode;
+}
+
+export const Button = ({ children, ...props }: ButtonProps) => {
+  return (
+    <button
+      className={button({ size: props.size, variant: props.variant })}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
