9a209e747b05487f78f253512e41229013d0d040
lucas vinicius <lucassilved@gmail.com>
2024-09-05 09:12:13 -0300

    feat: add input container and input error components

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,12 +1,28 @@
-import { forwardRef, InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes, ReactNode } from 'react';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface ErrorCompProps {
+  text: string | undefined;
+}
+
+export const ErrorComp = (props: ErrorCompProps) => {
+  return <p className="mt-[2px] text-xs text-red-400">{props.text}</p>;
+};
+
+export interface InputContainerProps {
+  children: ReactNode;
+}
+
+export const InputContainer = (props: InputContainerProps) => {
+  return <div className="flex flex-col gap-2">{props.children}</div>;
+};
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   return (
-    <div className="flex flex-col gap-2">
+    <>
       <label className="text-lg">{props.label}</label>
       <input
         type="email"
@@ -14,6 +30,6 @@ export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
         className="h-12 rounded-lg px-3 bg-zinc-900 text-sm focus:border focus:outline-none focus:border-solid focus:border-cyan-300"
         {...props}
       />
-    </div>
+    </>
   );
 });
