f1700aca61466955b189c05651c1cd1e6e4a1a60
lucas vinicius <lucassilved@gmail.com>
2024-09-05 09:16:03 -0300

    feat: add auth provider in router

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Bounce, ToastContainer } from 'react-toastify';
 import { SignUp } from '../../pages/sign-up';
 import { SignIn } from '../../pages/sign-in';
 import { NotFound } from './not-found';
+import { AuthProvider } from '../../context/auth-context';
+import { Home } from '../../pages/home';
 
 export function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/home" element={<SignUp />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
