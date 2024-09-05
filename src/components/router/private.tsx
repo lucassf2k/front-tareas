c458eb95890f62c5f1d210daca43abdadb9dc21b
lucas vinicius <lucassilved@gmail.com>
2024-09-05 09:15:32 -0300

    feat: add private route with auth context

--- src/components/router/private.tsx
+++ src/components/router/private.tsx
@@ -1,12 +1,17 @@
-import { ReactNode } from 'react';
+import { ReactNode, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../../context/auth-context';
+import { Loader } from '../loader';
 
 interface PrivateProps {
   children: ReactNode;
 }
 
 export function Private(props: PrivateProps) {
-  const isAuthentication = false;
-  if (!isAuthentication) return <Navigate to="/" replace />;
+  const authContext = useContext(AuthContext);
+  if (authContext.isLoading) {
+    return <Loader isLoading />;
+  }
+  if (!authContext.authenticated) return <Navigate to="/" replace />;
   return props.children;
 }
