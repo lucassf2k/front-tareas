9678cb29e28a7c2be424e3ae976a4c98c4e5c50b
lucas vinicius <lucassilved@gmail.com>
2024-09-05 21:33:57 -0300

    feat: add private route on home page

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Bounce, ToastContainer } from 'react-toastify';
 import { SignUp } from '../../pages/sign-up';
 import { SignIn } from '../../pages/sign-in';
 import { NotFound } from './not-found';
 import { AuthProvider } from '../../context/auth-context';
 import { Home } from '../../pages/home';
+import { Private } from './private';
 
 export function Router() {
   return (
@@ -13,7 +13,14 @@ export function Router() {
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/home" element={<Home />} />
+          <Route
+            path="/home"
+            element={
+              <Private>
+                <Home />
+              </Private>
+            }
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </AuthProvider>
