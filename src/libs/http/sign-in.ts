7de9b1c7812705aa7ae52c06692094e44f936f47
lucas vinicius <lucassilved@gmail.com>
2024-09-05 21:37:41 -0300

    fix: changing from receiving token via headers to coming in the body

--- src/libs/http/sign-in.ts
+++ src/libs/http/sign-in.ts
@@ -1,7 +1,15 @@
 import { HandleSignIn } from '../../domain';
 import { Axios } from './config';
 
-export const signIn = async (input: HandleSignIn) => {
-  const { headers } = await Axios.post('/sign-in', input);
-  return headers.authorizarion as string;
+export type SignInResponse = {
+  token: string;
+  user: {
+    id: string;
+    email: string;
+  };
+};
+
+export const signIn = async (input: HandleSignIn): Promise<SignInResponse> => {
+  const response = await Axios.post<SignInResponse>('/user/sign-in', input);
+  return response.data;
 };
