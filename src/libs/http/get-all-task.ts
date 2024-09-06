b9bc21e5be4f71c5f519889c26648a6303bfde8c
lucas vinicius <lucassilved@gmail.com>
2024-09-05 21:36:39 -0300

    feat: putting pagination in the task list call

--- src/libs/http/get-all-task.ts
+++ src/libs/http/get-all-task.ts
@@ -1,7 +1,12 @@
 import { Task } from '../../domain';
 import { Axios } from './config';
 
-export const getAllTask = async () => {
-  const output = await Axios.get<Task[]>('/tasks');
+export const getAllTask = async (
+  currentPage: number,
+  numberItemsPage: number = 10,
+): Promise<Task[]> => {
+  const output = await Axios.get<Task[]>(
+    `/tasks?page=${currentPage}&pageSize=${numberItemsPage}`,
+  );
   return output.data;
 };
